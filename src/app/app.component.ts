1e63ec14ea0954db0c24984166b7416fa503e200
tijana <tijana.couturier@web.de>
2022-07-17 19:16:14 +0200

    Add images and started to work on slide show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tijanacouturier';
+export class AppComponent implements OnInit {
+ images = ['apartment.jpg', 'furniture.jpg', 'interior-design.jpg', 'pool.jpg', 'pool-holiday.jpg', 'sunbeds.jpg' ];
+ currentImage = 0;
+ showImage = true;
+
+ ngOnInit(){
+   this.updateImage();
+ }
+
+ updateImage(){
+   setInterval(() => {
+     this.currentImage++;
+     this.currentImage = this.currentImage % this.images.length;
+     this.showImage = false;
+
+     setTimeout(() => {
+       this.showImage = true;
+     }, 10);
+
+    }, 8000);
+  }
 }
